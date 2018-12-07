b8a907c3b97c07746c28d939897c6b79404a3b31
emily.baker <emily.baker012@gmail.com>
2018-12-07 11:03:54 -0700

    Emily's Player class

---
+++ src/baker_player.py
@@ -0,0 +1,12 @@
+'''
+ File: baker_player.py
+ Author: Emily Baker
+ Brief: Player class for blackjack game
+ Last Modified: December 07, 2018
+'''
+from abstract_player import Abstract_Player
+
+class Baker(Abstract_Player):
+   def __init__(self):
+	super(Baker.self).__init__("Emily")
+

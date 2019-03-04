de4ac9a3189f68efb71a44c92df01ea9a1ae4c8a
robert.handley <robert.handley@dish.com>
2019-03-04 13:50:49 -0700

    Make changes according to team planning

--- src/card_class.py
+++ src/card_class.py
@@ -1,11 +1,25 @@
 import random
 
+
+
+class Card():
+    def __init__(self, suit, value, face_value):
+        self.suit = suit
+        self.value = value
+        self.face_value = face_value
+
+
+    def display_card(self):
+        print("{0:>2} of {1}".format(self.face_value, self.suit))
+
+
 class Deck ():
-    def __init__(self):
+    def __init__(self, num_decks=1):
+        self.deck_count = num_decks
         self.count=0
         self.suites_list = ["Hearts", "Clubs", "Spades", "Diamonds"]
         self.value_dict = {
-            "ace":11,
+            "A":11,
             "2": 2,
             "3": 3,
             "4": 4,
@@ -15,34 +29,46 @@ class Deck ():
             "8": 8,
             "9": 9,
             "10": 10,
-            "Jack": 10,
-            "Queen": 10,
-            "king": 10}
+            "J": 10,
+            "Q": 10,
+            "K": 10}
 
         self.deck_list=[]
+        for deck in range(num_decks):
+            self.deck_list.extend(self.__get_deck())
+        random.shuffle(self.deck_list)
+
+
+    def __get_deck(self):
+        new_deck = []
         for i in self.suites_list:
             for n in self.value_dict:
-                self.deck_list.append((i,n))
-                self.count+=1
+                new_deck.append(Card(i,self.value_dict[n],n))
+        random.shuffle(new_deck)
+        return new_deck
+
+
+    def reset_deck(self):
+        self.deck_list=[]
+        for deck in range(self.deck_count):
+            self.deck_list.extend(self.__get_deck())
+        random.shuffle(self.deck_list)
+
 
     def get_card(self):
-        self.num=random.randrange(0,self.count-1)
+        new_card = None
         try:
-            self.card=self.deck_list[self.num]
-            self.remove_card(self.card)
-        except:
-           print("Could not get card.")
-        return self.card
+            new_card = self.deck_list.pop()
+        except IndexError:
+            print("Out of Cards!\nCreating A New Deck")
+            self.reset_deck()
+            new_card = self.deck_list.pop()
 
-    def remove_card(self,card):
-        try:
-            self.deck_list.remove(card)
-            self.count-=1
-        except ValueError:
-            print("Card not found in deck, Could not be removed")
-    def display_card(self,card):
-        print("{1} of {0}".format(self.card[0], self.card[1]))
+        return new_card
+
+        
 
 test= Deck()
-card=test.get_card()
-test.display_card(card)
+for x in range(53):
+    card=test.get_card()
+    card.display_card()
